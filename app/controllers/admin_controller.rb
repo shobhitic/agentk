87944d598091479279441f299423f7f9a261c8e7
Shobhit <heyboxodi@gmail.com>
2020-12-06 15:23:13 +0530

    Added a way to update the app configuration

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,4 +4,24 @@ class AdminController < ApplicationController
   def index
     @app_configs = AppConfig.all
   end
+
+  def edit
+    @app_config = AppConfig.find params[:id]
+  end
+
+  def update
+    @app_config = AppConfig.find params[:id]
+    if @app_config.update app_config_params
+      redirect_to admin_path, notice: 'Configuration updated successfully.'
+    else
+      render :edit, alert: 'Please try updating again.'
+    end
+  end
+
+  private
+
+  def app_config_params
+    params.require(:app_config).permit(:value)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'admin', to: 'admin#index'
+  get 'admin/edit_config/:id', to: 'admin#edit', as: :edit_config
+  put 'admin/edit_config/:id', to: 'admin#update'
+  patch 'admin/edit_config/:id', to: 'admin#update'
 
   get 'p/:slug', to: 'articles#permalink', as: :permalink
 
